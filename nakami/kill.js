583ffbc06cbf9cbdc925b7cf5d9d4dcf1cb0db85
sakots <sakots8000@gmail.com>
2026-02-25 12:23:25 +0900

    コメント追加

--- nakami/kill.js
+++ nakami/kill.js
@@ -1,8 +1,12 @@
 function kill() {
+
+  // 入力した文字を取得
   const kill_class = document.pokemon.char.value;
   console.log (kill_class);
   let kill_class_split = kill_class.split('');
   console.log (kill_class_split);
+
+  // 入力した文字のクラスを持つ要素を消す
   for (let i =0; i < kill_class_split.length; i++) {
     let target_class = kill_class_split[i];
     let target_char = document.getElementsByClassName(target_class);
