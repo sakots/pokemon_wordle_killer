4e3625df7360cdb56f363090e98c4251e161a8f0
sakots <sakots8000@gmail.com>
2026-03-06 12:44:31 +0900

    fix: ハイライト機能をAND検索に

--- nakami/kill.js
+++ nakami/kill.js
@@ -38,19 +38,34 @@ function highlight() {
 
   // 入力した文字を取得
   const highlight_class = document.pokemon_highlight.char.value;
-  console.log (highlight_class);
+  console.log(highlight_class);
   let highlight_class_split = highlight_class.split('');
-  console.log (highlight_class_split);
+  console.log(highlight_class_split);
 
-  // 入力した文字のクラスを持つ要素をハイライト
-  for (let i =0; i < highlight_class_split.length; i++) {
-    let target_class = highlight_class_split[i];
-    let target_char = document.getElementsByClassName(target_class);
-    console.log (target_char);
-    for (let j = 0; j < target_char.length; j++) {
-      target_char[j].style.backgroundColor = "yellow";
-    }
+  // まず既存のハイライトを解除
+  highlight_reset();
+
+  if (highlight_class_split.length === 0) {
+    // 空だったら何もしない
+    return;
   }
+
+  // 全てのリストアイテムを調べ、クラス名文字列が
+  // 入力したすべての文字を含むものだけをハイライト
+  const items = document.querySelectorAll('#target_pokemon li');
+  items.forEach(item => {
+    const clsStr = item.className; // スペース区切りの全クラス名
+    let hasAll = true;
+    for (let ch of highlight_class_split) {
+      if (clsStr.indexOf(ch) === -1) {
+        hasAll = false;
+        break;
+      }
+    }
+    if (hasAll) {
+      item.style.backgroundColor = 'yellow';
+    }
+  });
 
   // フォームをクリアしておく
   document.pokemon_highlight.char.value = "";
